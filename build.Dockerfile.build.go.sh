9020e2d77902a937f8acd9ab3ddeae410f3243fb
soongao <soongao@tencent.com>
2024-11-02 17:30:11 +0800

    制作一个编译的镜像

---
+++ build.Dockerfile.build.go.sh
@@ -0,0 +1,32 @@
+
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "当前时间是: [$current_time]"
+
+docker rm $(docker ps -aqf "ancestor=my-markdown-blog")
+docker ps -a  | grep my-markdown-blog
+
+docker build --build-arg CURRENT_TIME="${current_time}"  --tag my-markdown-blog -f Dockerfile.build.go.1 .
+
+
+md_path="/Users/fullname/doc/work/gamestudio/bookmark/docs"
+custom_cmd="/bin/bash"
+custom_cmd="make run"
+
+
+# --rm 退出后，自动删除容器
+docker run --rm  -it -p 5006:5006 -v ./:/app  -v ${md_path}:/md  my-markdown-blog $custom_cmd 
+# docker run --rm  -it -p 5006:5006 -v ./:/app  my-markdown-blog /bin/bash 
+
+# 启动 web服务. 运行之后访问地址 http://localhost:5006
+# make run
+
+# 编译, 在 bin 目录生成当前系统的压缩包，如：markdown-blog-v1.1.0-darwin-amd64.tar
+# make
+
+# 打包, 在 package 目录生成当前系统的压缩包，如：markdown-blog-v1.1.0-darwin-amd64.tar
+# make package
+
+
+# 生成 Windows、Linux、Mac 的压缩包
+# 在 package 生成压缩包，如：markdown-blog-v1.1.0-darwin-amd64.tar markdown-blog-v1.1.0-linux-amd64.tar.gz markdown-blog-v1.1.0-windows-amd64.zip
+# make package-all
