44ddab745c21486c9c1540fd400fbd904c16869d
soon <wpfchina@qq.com>
2024-11-11 14:07:25 +0800

    shell

--- Dockerfile.build.go.2.sh
+++ Dockerfile.build.go.2.sh
@@ -11,7 +11,7 @@ docker build  --tag ${prod_image_name} -f Dockerfile.build.go.2 .
 export md_path="/Users/fullname/doc/work/gamestudio/bookmark/docs"
 
 echo "docker run  -it -p 5006:5006  -v ${md_path}:/md --name my-test  ${prod_image_name}"
-docker run  -dit --rm -p 5006:5006  -v ${md_path}:/md --name=my-test  ${prod_image_name}
+docker run  -it --rm -p 5006:5006  -v ${md_path}:/md --name my-test  ${prod_image_name}
 docker exec -it my-test /data/app/markdown-blog
 
 # scratch 体积更小
